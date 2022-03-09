a57c0001cd7973c9007e1822ddd4a73b71cb17f6
H1ghBre4k3r <h1ghbre4k3r@dev.bre4k3r.de>
2022-03-09 15:09:31 +0100

    Add script for performing many evaluation steps at once

---
+++ scripts/evaluate.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+
+import argparse
+import subprocess
+
+parser = argparse.ArgumentParser(description='Perform stuff')
+parser.add_argument("-e", type=str, required=True, help="Path to exectuable")
+parser.add_argument("-s", type=str, required=True, help="Path to script")
+parser.add_argument("-o", type=str, required=True, help="Path to output")
+parser.add_argument("-c", action=argparse.BooleanOptionalAction, default=False, help="Consensus")
+noises = [0, 0.0001, 0.0002, 0.0005, 0.001, 0.002, 0.005, 0.01, 0.02, 0.05]
+
+args = parser.parse_args()
+subprocess.call([args.e, "-c", args.s, "-o", args.o])
+
+for noise in noises:
+    for i in range(10):
+        subprocess.call([args.e, "-c", args.s, "-o", args.o, "-n", str(noise), f"-consensus={args.c}"])
