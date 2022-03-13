d26b744fdb9c633961a96056ebd96c197c116564
H1ghBre4k3r <h1ghbre4k3r@dev.bre4k3r.de>
2022-03-13 23:20:13 +0100

    Add script for generating graph by noise

--- scripts/generate-graph-by-noise.py
+++ scripts/generate-graph-by-noise.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python3
 
+
 import argparse
 import json
 from cmath import nan
@@ -31,72 +32,76 @@ class Mode(Enum):
 parser = argparse.ArgumentParser(description='Perform stuff')
 parser.add_argument("-f", type=str, required=True, help="Path to file")
 parser.add_argument("-o", type=str, help="Path to output file")
-parser.add_argument("-n", type=str, required=True, help="Noise")
+parser.add_argument("-p", nargs="+", required=True, help="Participants")
 parser.add_argument("-c", action=argparse.BooleanOptionalAction, default=False, help="Consensus")
 parser.add_argument("-m", type=Mode, required=True, choices=list(Mode), help="Mode: runtime or collisions")
 parser.add_argument("-d", type=str, required=True, help="Name of detail")
-parser.add_argument("-p", nargs="+", required=True, help="Participants")
+parser.add_argument("-t", type=str, required=True, help="Value of tau")
 parser.add_argument("-l", type=str, default="", help="Label for legend")
+
 args = parser.parse_args()
 
 file = json.loads(open(args.f).read())
 
 summaries = {}
 
+# participant -> tau -> noise -> consensus
+
 for (n, p) in file.items():
     if n not in args.p:
         continue
-    for (tau, ns) in p.items():
-        if args.n not in ns:
+    if args.t not in p:
+        continue
+    tau = p[args.t]
+    for participant in tau:
+        if participant not in summaries:
+            summaries[participant] = {}
+        noised = tau[participant]
+        if str(args.c).lower() not in tau[participant]:
             continue
-        noised = ns[args.n]
-        if str(args.c).lower() not in noised:
-            continue
-        if tau not in summaries:
-            summaries[tau] = {}
         consensus = noised[str(args.c).lower()]
         mode = consensus[args.m.value]
         detail = []
-        for i in range(len(mode)):
-            detail.append(mode[i][args.d])
-        summaries[tau][n] = {
+        for i in range (len(mode)):
+            detail.append(mode[i][args.d]) 
+        summaries[participant][n] = {
             "mean": np.mean(detail),
             "ci": st.t.interval(alpha=0.95, df=len(detail)-1, loc=np.mean(detail), scale=st.sem(detail))
         }
 
-x = [int(k) for k in summaries.keys()]
+x = [float(k) for k in summaries.keys()]
 x.sort()
-x = [str(k) for k in x]
+x = [str(k) if k != 0 else "0" for k in x]
 ys = {}
-
-for tau in x:
-    for p in args.p:
-        if p not in ys:
-            ys[p] = {
+for n in x:
+    for participant in args.p:
+        if participant not in ys:
+            ys[participant] = {
                 "mean": [],
                 "lower": [],
                 "upper": []
             }
-        if p not in summaries[tau]:
-            ys[p]["mean"].append(nan)
-            ys[p]["lower"].append(nan)
-            ys[p]["upper"].append(nan)
+        if summaries[n][participant] is None:
+            ys[participant]["mean"].append(nan)
+            ys[participant]["lower"].append(nan)
+            ys[participant]["upper"].append(nan)
         else:
-            ys[p]["mean"].append(summaries[tau][p]["mean"])
-            if np.isnan(summaries[tau][p]["ci"][0]):
-                ys[p]["lower"].append(summaries[tau][p]["mean"])
+            ys[participant]["mean"].append(summaries[n][participant]["mean"])
+            if np.isnan(summaries[n][participant]["ci"][0]):
+                ys[participant]["lower"].append(summaries[n][participant]["mean"])
             else:
-                ys[p]["lower"].append(summaries[tau][p]["ci"][0])
-            if np.isnan(summaries[tau][p]["ci"][1]):
-                ys[p]["upper"].append(summaries[tau][p]["mean"])
+                ys[participant]["lower"].append(summaries[n][participant]["ci"][0])
+            if np.isnan(summaries[n][participant]["ci"][1]):
+                
+                ys[participant]["upper"].append(summaries[n][participant]["mean"])
             else:
-                ys[p]["upper"].append(summaries[tau][p]["ci"][1])
+                ys[participant]["upper"].append(summaries[n][participant]["ci"][1])
 
 i = 0
 for (n, y) in ys.items():
-    plt.plot(x, y["mean"],linestyle=linestyle_tuple[i], label=f"{n} {args.l}")
+    plt.plot(x, y["mean"], linestyle=linestyle_tuple[i], label=f"{n} {args.l}")
     plt.fill_between(x, y["lower"], y["upper"], alpha=.4, label="95% CI")
-    i = (i+1) % len(linestyle_tuple)
+    i = (i+1)%len(linestyle_tuple)
 
 plt.xlabel("N° of participants")
 plt.ylabel(args.m.value)
